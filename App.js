94df83b90cdf9145f416f9a2601a195b44834c07
arsalan <arsalan.rk8@gmail.com>
2022-02-21 20:06:35 +0530

    fonts

--- App.js
+++ App.js
@@ -2,10 +2,28 @@ import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
 
+import {
+  useFonts as useOswald,
+  Oswald_400BRegular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400BRegular } from "@expo-google-fonts/lato";
+
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400BRegular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400BRegular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
