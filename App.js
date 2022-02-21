842c6869ed6877cbb76ad546566d44b4db24dfa3
arsalan <arsalan.rk8@gmail.com>
2022-02-21 20:20:48 +0530

    f2

--- App.js
+++ App.js
@@ -4,20 +4,20 @@ import { ThemeProvider } from "styled-components/native";
 
 import {
   useFonts as useOswald,
-  Oswald_400BRegular,
+  Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
-import { useFonts as useLato, Lato_400BRegular } from "@expo-google-fonts/lato";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
-    Oswald_400BRegular,
+    Oswald_400Regular,
   });
 
   const [latoLoaded] = useLato({
-    Lato_400BRegular,
+    Lato_400Regular,
   });
 
   if (!oswaldLoaded || !latoLoaded) {
