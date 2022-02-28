c8430e93b43d476df2e45fd65a4047760e7846cc
arsalan <arsalan.rk8@gmail.com>
2022-02-28 18:53:02 +0530

    navigation bar

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { Text } from "react-native";
 import { ThemeProvider } from "styled-components/native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 import {
   useFonts as useOswald,
@@ -9,8 +13,9 @@ import {
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infrastructure/theme";
-import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
+const Setting = () => <Text>Settingsho</Text>;
+const Maps = () => <Text>mapssss</Text>;
 export default function App() {
   // snytax for font.. from->react native expo font  github
   const [oswaldLoaded] = useOswald({
@@ -24,12 +29,20 @@ export default function App() {
     return null;
   }
 
+  const Tab = createBottomTabNavigator();
+
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
-        <ExpoStatusBar style="auto" />
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Restaurant" component={RestaurantsScreen} />
+            <Tab.Screen name="Setting" component={Setting} />
+            <Tab.Screen name="Map" component={Maps} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
