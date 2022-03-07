e8a5e02539911cf2687db5d83be2cc5b721e9ec3
arsalan <arsalan.rk8@gmail.com>
2022-03-07 14:19:56 +0530

    camelize

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,4 +1,6 @@
 import { mocks } from "./mock";
+// camelize is npm package to help in keeping the name format of fake api and  ours same
+import camelize from "camelize";
 export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   // console.log(mocks[location]);
 
@@ -12,12 +14,18 @@ export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   });
 };
 // when using new Promise its awaiting something in the future. so it has to return something awaiting--
+
+const restaurantTransform = (boy) => {
+  return camelize(boy);
+};
+restaurantsRequest()
+  .then(restaurantTransform)
+  .then((t) => {
+    console.log(t);
+  })
+  .catch((err) => {
+    // console.log("error- " + err);
+  });
+
 //    .then() is used for showing ,is it is resolved -result
 //    .catch() is used for showing rejects -error
-restaurantsRequest()
-  .then((result) => {
-    // console.log("found ");
-  })
-  .catch((result) => {
-    // console.log("error- not found");
-  });
