044252951af838a7e2b70b0231fd8d460500b625
arsalan <arsalan.rk8@gmail.com>
2022-03-08 18:05:52 +0530

    recieving context

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -5,5 +5,9 @@ import { restaurantsRequest, restaurantTransform } from "./restaurants.service";
 export const RestaurantsContext = createContext();
 
 export const RestaurantsContextProvider = ({ children }) => {
-  return <RestaurantsContext.Provider>{children}</RestaurantsContext.Provider>;
+  return (
+    <RestaurantsContext.Provider value={{ restaurants: [1, 2, 3] }}>
+      {children}
+    </RestaurantsContext.Provider>
+  );
 };
