d8509e8e466af34cb20656631d78cc5cb9813c4a
Arsalan <arsalan.rk8@gmail.com>
2022-03-13 16:41:38 +0530

    pics

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -11,7 +11,7 @@ export const RestaurantsContextProvider = ({ children }) => {
   const [restaurants, setRestaurants] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-
+  console.log(error);
   //  the restaurants is an empty array but value is set by in retrieveRestaurants by setRestaurants(results)), can also do this  setRestaurants([1, 2, 3]);
   //results  has the mock api value. do console.log(results);
 
@@ -19,6 +19,8 @@ export const RestaurantsContextProvider = ({ children }) => {
   const retrieveRestaurants = () => {
     setIsLoading(true);
     setTimeout(() => {
+      // restaurantsRequest() or ("false location") => this is getting its value restaurants.service in which we have already given alocation .
+      //  if we overwrite restaurantsRequest("false location") - will show error because of new promise reject
       restaurantsRequest()
         .then(restaurantsTransform)
         .then((results) => {
