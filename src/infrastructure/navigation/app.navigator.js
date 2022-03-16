496542f26cea6951168888f04155932d36bfed2b
Arsalan <arsalan.rk8@gmail.com>
2022-03-16 19:22:59 +0530

    independent navigation

---
+++ src/infrastructure/navigation/app.navigator.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Text } from "react-native";
+
+// import of navigation bar and icons .
+import { Ionicons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
+import { SafeArea } from "../../components/Utility/safe-area-component";
+
+// part of Tab. navigation syntax
+const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Restaurants: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings",
+};
+
+//Screens
+const Setting = () => (
+  <SafeArea>
+    <Text>setting</Text>
+  </SafeArea>
+);
+const Maps = () => (
+  <SafeArea>
+    <Text>mapssss</Text>
+  </SafeArea>
+);
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    headerShown: false,
+
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+  };
+};
+
+export const AppNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={createScreenOptions}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+        }}
+      >
+        <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+
+        <Tab.Screen name="Map" component={Maps} />
+
+        <Tab.Screen
+          name="Settings"
+          component={Setting}
+          // options={{ headerShown: false, tabBarBadge: 3 }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
