7c4717edd48e5e93b40530c61d9d48809dcbb214
Arsalan <arsalan.rk8@gmail.com>
2022-03-17 13:36:34 +0530

    up-swipe

--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -1,24 +1,30 @@
 import React from "react";
 import { Text } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from "@react-navigation/stack";
 import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
 
 const RestaurantStack = createStackNavigator();
 
 export const RestaurantsNavigator = () => {
   return (
-    <RestaurantStack.Navigator headerMode="none">
-      <RestaurantStack.Screen
-        name="RestaurantDetail"
-        component={() => <Text>waehoawjkblagslkj</Text>}
-        // the name is the header name
-      />
+    <RestaurantStack.Navigator
+      headerMode="none"
+      screenOptions={{ ...TransitionPresets.ModalPresentationIOS }}
+    >
       <RestaurantStack.Screen
         name="Restaurants"
         component={RestaurantsScreen}
         // options={{
         //   headerShown: false,
         // }} // the name is the header name
+      />
+      <RestaurantStack.Screen
+        name="RestaurantDetail"
+        component={() => <Text>faweaf</Text>}
+        // the name is the header name
       />
     </RestaurantStack.Navigator>
   );
