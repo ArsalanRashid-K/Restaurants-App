8f5bb041d5cf2b7c42b05a6334810bc424322257
Arsalan <arsalan.rk8@gmail.com>
2022-03-23 00:47:47 +0530

    button add

--- src/features/account/components/account.styles.js
+++ src/features/account/components/account.styles.js
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "styled-components/native";
+import { Button } from "react-native-paper";
+
+import { colors } from "../../../infrastructure/theme/colors";
 
 export const AccountBackground = styled.ImageBackground.attrs({
   source: require("../../../../assets/home_bg.jpg"),
@@ -8,3 +11,17 @@ export const AccountBackground = styled.ImageBackground.attrs({
   align-items: center;
   justify-content: center;
 `;
+
+export const AccountCover = styled.View`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.3);
+`;
+export const AccountContainer = styled.View`
+  background-color: rgba(255, 255, 255, 0.7);
+  padding: ${(props) => props.theme.space[4]};
+  margin-top: ${(props) => props.theme.space[2]};
+`;
+
+
--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Button } from "react-native-paper";
 
-import { AccountBackground } from "../components/account.styles";
+import {
+  AccountBackground,
+  AccountCover,
+  AccountContainer,
+} from "../components/account.styles";
 
 export const AccountScreen = () => {
-  return <AccountBackground />;
+  return (
+    <AccountBackground>
+      <AccountCover />
+      <AccountContainer>
+        <Button
+          icon="lock-open-outline"
+          mode="contained"
+          onPress={() => console.log("Pressed")}
+        >
+          Login
+        </Button>
+      </AccountContainer>
+    </AccountBackground>
+  );
 };
