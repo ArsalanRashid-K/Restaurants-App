701f9eb7da4884dbbcd7444561cbaaebe5601419
Arsalan <arsalan.rk8@gmail.com>
2022-04-10 21:14:20 +0530

    camera working

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,4 +1,34 @@
-import React from "react";
-import { Text } from "react-native-paper";
+import React, { useState, useEffect, useRef } from "react";
+import { View } from "react-native";
+import { Text } from "../../../components/typography/text.component";
+import { Camera } from "expo-camera";
+import styled from "styled-components/native";
 
-export const CameraScreen = () => null;
+const ProfileCamera = styled(Camera)`
+  flex: 1;
+`;
+
+export const CameraScreen = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const cameraRef = useRef();
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <ProfileCamera
+      ref={(camera) => (cameraRef.current = camera)}
+      type={Camera.Constants.Type.front}
+    ></ProfileCamera>
+  );
+};
